70604309fc7f779d3be1b409918f7c748b75491d
Simamkele20 <Princetancu06@gmail.com>
2024-02-20 13:49:04 +0200

    HomeContent

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -2,14 +2,13 @@
     <nav class="navbar  navbar-expand-lg navbar-dark bg-black bg-gradient sticky-top" id="Shift">
         <div class="container-fluid">
             <route-link to="/" class="nav-link">
-             <img src="https://lh3.googleusercontent.com/fife/AGXqzDmNt4lydKZAIeyIx8Ka-NP6o1drMk1rAZnZBzylKKp84KKIwuS9wvQt_axxwRH0IiLx-KAEUR50fuK46Nggx3kb6_DHPTJDBAVVM3Xb5LeWgXyosg0Zj8j5hk8JbCGHJCB-ZbCJKT0whILL9zX-566rYAJ3Yo1V6Uwjcj0z6iYxeL5qdJ-xWrDqTI9MXOcZh7tCW0eRnLbEct1v3EOhFidZHW3JuaZ_jZfCJTvvFih-qFtjsusLE6eAgDwL5dP5Z2xi_bxYXNNgyoDYz2_A0x-1ZZHhs4S29sBU18jPTNiPopjKv81y9Q_L1sfuHXzltbur4f26NgxTAPNIKUrWftpm-66QDCMPgpFI3oKTOumtxL4l2SVGMs94Q20G9G2X09ntPrPfop1LjPlENqdUNZenM5-x3CEUI9SLesVnjxWrdkbD5IPkLEevY0Sbec4ql4zt_KrH90wNzvaFrKUa85BjgqA81R6DoSokMYSWC5Z5-6j_rxwcAYhheEzNVAUb4xi_KP3zMPYwbS0Z3DIi4cYCrOxlY9JOV2GSwP7cBUiQPFSOzik8maB4DZsRUoe1nH57vcEEPU9yz7z2TGYX2e0ViFJNGJFiqbeikUI3_YwXh2udPIH6ay2ML6rE6ptpLCi3BGEi0X9-Yp0qEGy3snHpnyZVKANwVvpachUYWQDCPiPhMpEwZZ408o3MUldzpGCjGrfjbHWpzQgiNLDTI0K3F53LKTAUd9hyTftLpz1nFyb47k8fg4CZLKyeVI1Ar-sQ01-lFh8UxWsXx1LPWVNdgBwxx0dHcgDQ8YbItpYhuyskzcIdeepnMsipdW5H-UswqwpBWuZzVnWYGv4NWGe31WEpb1GR8I4FMGNbRJRHmImiHeclmi2NcJGp41axPF9VFpL8ul_3nmAUNEG0ul5pxElgcu7t8UWO4tARFGZdhDKX7jpGTKE0gCg6iTA2nu4agyjq17KEs_RsSkpx2ARgc6xS4uUQs341aeS9XraAM-_elLjwpgBsHFpGOA-pGVmzJdhh1sM0f58hpL5CZuMssvLzLfqPPKmKAWqaCeoBArckJmgrlZwrm0zJD_buRxia3PliCznjcKpNTwGL8c-_Gejow_EnKggg2SkVRfCxkEcr6N-YIHtNa_70u8rEAF8STUieSjzKeRjMNjlOBTT4wKYbr8fNuVliRD6z229Kr308UixHlOP_hlU6ehhvAAisRcASB2CZJ7yG6W86DiBVFA2u6BjPP3UiWk8EZj1XzXBRdTBj8okL_3RTr3LRHtuvM3H4a54Yu4u1vdWu73kt0ER1AkXjWf5-eMsy8PIT9R8k7kqsGBDxGZoHslqbbNhWbIdbGLhiSxaA7AnfHFwXNtWXVbToOo8FtABwl3RkDzjibwma17S8MlF2Z3yK8_RN_FFP0GcO2LcyS1cVVuy4SjEHZROS0FcW9yDc-zOvfymylaespw3dbPS4bvWm2eB9mbSfQ8oyU2yGZQOpDUI3-mIBLvcU5pleqEPajIdNOeSoiQibXdQirHbLTUOt51niHyk5xVc=w554-h414-p-k-nu"    class="img-fluid w-50 img-thumbnail " loading="lazy" alt="Logo" id="Navimg">   
-            </route-link>
-            <button class="navbar-toggler  mt-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+<img src="https://lh3.googleusercontent.com/fife/AGXqzDl-jqJetVRPfI-pyTKxkd64Ffm5hB8b-Gcr73NQ_FUusUViXTnQjGhWtW0C0LsORFKmxEbxGfESE3MYJN8MJqau7tcEnu-ZswBaby3waeB3A0ENq0dlw40uz8wddyL2lgYTMDNNiiSQ19eglAMBobORyOU6cf56xh6J0_JjsWuvCWzGM2n59nfChjxbC1YwWaFSs5ewVRXWrJ6w_94zK-l1y7TOEBATvGfmW3Sg7Az55uR6VyJGNrkyVwH2mGIK-JIOg53Hs2Sqw-f--JoE1_3w69C-6rFK6Lkv64--K2aZINey_ZmEd5ZX752QE7s4ujksd5zC4IVoJ8pHLDbH71UZgU1qWvkGKlCDGN4pX1b22f6MOMEOefYNDCmA3JeXfK6ub6goBJVfAfBryZNoIXum4Pis3FgGACtKA0aiTHl-MqsgRCov_NpCWnzur0Ie3_qJBFjsxvDVOjlFuBuisAeh16UUBb197-bXm0fhuZHfo0gi1mQeZ227l4kcRMoUJut9rNMVQqYecfolKjQEP18xphMo886GAEV_hJZXgmYFszTd-B7hklv2Ypkp6Iu-as1E3LCWD7r8lYjq9PF-iQRxGNJtIVIYC3cbYYkWR3UlRvRLPlU1Ix3h3YlUns85pW_dkiTkN3FOvDJuaveIuCKhlrPsREQonMjnfnYcPdzOrpJNBP_CPaIjcpRg5N0ZwAL5fzG2I4KhSMxsoM1CL2oxqJveyWRK85m0Iiof-L_P_pVKyLRoJiQrkA2g95Jv71uy0XEjaPGw6HvDKoe5XaNTl3MzwJWXsWm5dHkXjE5fFb9HouXr-ebLP1cwBw_VrrnyfSJjFitVhKrMoz2ztSFyH4mRosesQOMB1-eQlT6cAQgKygPF0J5iXXZ-oUsU2B-mfHLtVaLeyWmfRmrpiFSK4ZVGaPgP9I5YP1yg4aWeb370cbzM1DnLSx9rRrlfCiDo0KB50IiAx0m0PV2z6vxqz3eDemhSlM-c4_OD3O1kvhSl1jLpeZ6pfqkA_F_gjPYsf-25CWvxEFS9nq7hsUJDocIDx35CyZE8DlsPxx9J0KdoIjyy8a3JY_3fr6OwRrIMjEtr8MUC1TMnYmjzP8udc7otcozYM7wnvcRjrjtwbk-DkcANz8WYTrsqErrZMn_ytoge2n9FDe1Thb9yTzhCtPPvgsX_maGTmcwKLhVGT4b5zyk8MmIlEr_OGPKQ0vJC1pQBSZC0LlHEDAaP01B0gckRUwKyIOB8OMyll9F8_AFlVjXS2-cPw7Df3XYKBLZdL64c9zYmBD7okbyCikGuTtbjyt61zeemZPs1V8brxsC1Qt4YSAhJ5Vb8crrxfAD6xlyZn6rMW57ZiYg_IXTI4K1WeB6GRH-cgF-Qrpcf2mkArkVHmHPKfOswTa1kB6tQ-KxsYR1RRD_PQ1-hnsxlx9IdbCw72HdLUB6-rFcOGU-B4wzfXmnsvVc1H1852u2-eW19ScCe3hEjOd5F89n4EZJ7Wja2EgPJNcu1ew9gqBci_EPcx_2vyUsorvLvh8AnEaszYw=w554-h414-p-k-nu" alt="Logo" class="w-25">         </route-link>
+            <button class="navbar-toggler mt-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse  " id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto  mb-lg-0 mx-end">
+            <div class="collapse navbar-collapse text-center  " id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto text-center mb-lg-0 mx-end">
                     <li class="nav-item">
                         <router-link to="/" class="nav-link">Home  |</router-link>
                     </li>
@@ -37,4 +36,5 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
