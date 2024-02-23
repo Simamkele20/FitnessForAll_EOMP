6c4a760de3cbd68aa341e06a69cecc8dad67e619
fortuinp <pinda.fortuin@gmail.com>
2024-02-23 15:06:39 +0200

    model

--- Backend/model/Products.js
+++ Backend/model/Products.js
@@ -1,1 +1,81 @@
-//Products
+import {connection as db} from "../config/index.js"
+ class Products{
+    fetchProducts(req,res){
+        const qry =`
+        SELECT prodID,prodName,prodQuantity, prodAmount,prodDescription,prodUrl,userID
+        FROM Products
+        
+        `
+        db.query(qry, (err, results) => {
+            if (err) throw err;
+            res.json({
+              status: res.statusCode,
+              results,
+            });
+          }); 
+    }
+    fetchProduct(req,res){
+        const qry =`
+        SELECT prodID,prodName,prodQuantity, prodAmount,prodDescription,prodUrl,userID
+        FROM Products
+        WHERE prodID=${req.params.id}
+        `
+        db.query(qry, (err, result) => {
+            if (err) throw err;
+            res.json({
+              status: res.statusCode,
+              result,
+            });
+          }); 
+    }
+    addProduct(req,res){
+        const qry=`INSERT INTO Products SET ?;`;
+
+        db.query(qry,[req.body] , (err)=>{
+          if(err) throw err
+          res.json({
+            status:res.statusCode,
+            msg:'new product was added'
+          })
+
+
+        })
+    }
+
+    async updateProduct(req,res){
+      const data=req.body
+      if(data?.userPwd){
+        data.userPwd=await hash(data?.userPwd,8)
+      }
+      const qry=`
+    UPDATE Products 
+    SET ?
+    WHERE prodID=${req.params.id};`
+    db.query(qry, [data], (err) => {
+        if (err) throw err 
+          res.json({
+            status: res.statusCode,
+            msg: "products updated",
+          
+          })
+        })
+  
+    }
+  
+    deleteProduct(req,res){
+      const qry=`
+     DELETE FROM Products
+     WHERE prodID=${req.params.id}
+  `
+     db.query(qry, (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg:"product deleted"
+      });
+    });
+    }
+ }
+ export{
+    Products
+ }
--- Backend/model/Users.js
+++ Backend/model/Users.js
@@ -1,1 +1,104 @@
-//user
+import { dbconnection as db } from "../config/index.js";
+import { hash, compare } from "bcrypt";
+
+class Users {
+  fetchUsers(req, res) {
+    const qry = `
+        SELECT UserID,firstName,lastName,userAge,gender,emailAdd,userPass,userRole,userProfile
+        FROM Users
+        `;
+
+    db.query(qry, (err, results) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        results,
+      });
+    });
+  }
+  fetchUser(req, res) {
+    const qry = `
+        SELECT UserID,firstName,lastName,
+        userAge,gender,emailAdd,userPass,userRole,userProfile
+        FROM Users
+        WHERE userID = ${req.params.id}
+        `;
+
+    db.query(qry, (err, result) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        result,
+      });
+    });
+  }
+  async createUser(req, res) {
+    //payload
+    let data = req.body;
+    data.userPwd = await hash(data?.userPwd, 8);
+    let user = {
+      emailAdd: data.emailAdd,
+      userPwd: data.userPwd,
+    };
+    const qry = `
+      INSERT INTO Users
+      SET ?;
+      `;
+    db.query(qry, [data], (err) => {
+      if (err) {
+        res.json({
+          status: res.statusCode,
+          msg: "already exists.please use another email address",
+        });
+      } else {
+        //create token
+        let token = createToken(user);
+        res.json({
+          status: res.statusCode,
+          token,
+          msg: "You're registered",
+        });
+      }
+    });
+  }
+
+
+  async updateUser(req,res){
+    const data=req.body
+    if(data?.userPwd){
+      data.userPwd=await hash(data?.userPwd,8)
+    }
+    const qry=`
+  UPDATE Users 
+  SET ?
+  WHERE userID=${req.params.id};`
+  db.query(qry, [data], (err) => {
+      if (err) throw err 
+        res.json({
+          status: res.statusCode,
+          msg: "user updated",
+        
+        })
+      })
+
+  }
+
+  deleteUser(req,res){
+    const qry=`
+   DELETE FROM Users
+   WHERE userID=${req.params.id}
+`
+   db.query(qry, (err) => {
+    if (err) throw err;
+    res.json({
+      status: res.statusCode,
+      msg:"deleted"
+    });
+  });
+  }
+}
+
+
+export{
+    Users
+}
