3d1be04f8cfc3b660a38e2527c940ecb8bd9ae84
Simamkele20 <Princetancu06@gmail.com>
2024-02-23 11:17:14 +0200

    ProductContent

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="">
-
-
-    <div class="welcome">
-      <h6 class="heading1 text-white ">Empowering every body</h6>
-      <h1 class="heading2 text-white   "> fitness for All</h1>
-      <a class="btn btn-light " href="#" role="button">Shop Now</a>
-    </div>
-
+  <div>
+      <div class="welcome row">
+        <div class="col">
+          <h6 class="heading1 text-white ">Empowering every body</h6>
+          <h1 class="heading2 text-white   "> fitness for All</h1>
+        </div>
+        <div class="HomeBtn col-3">
+          <a class="btn btn-light " href="#" role="button">Shop Now</a>
+        </div>
+      </div>
     <div class="text-center text-black">
       <h3 class=" heading3 mt-5 "> New Arrivals</h3>
-    <h1> <i class="bi bi-arrow-left"></i> </h1>  
-    <h1> <i class="bi bi-arrow-right "></i> </h1>  
-
     </div>
 
 
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,6 +1,30 @@
 <template>
-    <div class="about">
-      <h1>This is an about page</h1>
-    </div>
-  </template>
+  <div class="container mt-3 ">
+      <div class="bg-black text-center">
   
+      </div>
+
+
+          <div class=" prodBtn row text-end">
+              <div class="col">
+                  <input type="text" placeholder="Search Product by name" class="form-control" >
+              </div>
+              <div class="col">
+                  <button class=" btn btn-dark"> Sorting by Price</button>
+              </div>
+          </div>
+      </div>
+  
+  
+</template>
+
+
+<script>
+export default {
+  name: "ProductsView"
+}
+</script>
+
+
+<style src="../assets/css/style.css">
+</style>
